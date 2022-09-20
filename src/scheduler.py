141ad0211d8dcfdce9bfe73b4ebbe1a3a3e6109a
Neopect <tyler.moen@earthlink.net>
2022-09-20 12:49:44 -0500

    test

---
+++ src/scheduler.py
@@ -0,0 +1,25 @@
+import os
+
+global task
+task = []
+
+
+class Schedular():
+    
+    def __init__(self):
+        pass
+    
+    def 
+    
+    
+
+class Queue():
+    
+    def __init__(self):
+        pass
+    
+    
+    def assign(self):
+        pass
+    
+    def
